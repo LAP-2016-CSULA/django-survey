ba374f40479ef3c164c42aa4b63a48224ed807de
isitso <ryan033111@gmail.com>
2015-11-06 00:36:18 -0800

    display number of questions, number of choices in detail template

--- survey/models.py
+++ survey/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Question(models.Model):
+    """ Question model. """
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
     
@@ -9,6 +10,8 @@ class Question(models.Model):
         return self.question_text
 
 class Choice(models.Model):
+    """ Choice model. """
+    # ForeignKey is many-to-one or the other way around
     question = models.ForeignKey(Question)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
@@ -17,7 +20,11 @@ class Choice(models.Model):
         return self.choice_text
     
 class Survey(models.Model):
+    """ Survey model. """
     title = models.CharField(max_length=200, default='')
+    # Since a survey contains many different questions, and a question
+    # can be belong to many different survery, ManyToManyField is
+    # needed in this case 
     questions = models.ManyToManyField(Question, related_name='surveys')
     
     def __str__(self):
--- survey/views.py
+++ survey/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render
 from django.views import generic
 from .models import Question, Choice, Survey
 
-# Create your views here.
+
 class QuestionIndexView(generic.ListView):
+    """ Question Llist. """
     template_name = 'survey/questions.html'
     context_object_name = 'latest_question_list'
     
@@ -12,6 +13,7 @@ class QuestionIndexView(generic.ListView):
         return Question.objects.order_by('-pub_date')[:5]
     
 class SurveyIndexView(generic.ListView):
+    """ Survey list. """
     template_name = 'survey/surveys.html'
     context_object_name = 'latest_survey_list'
     
@@ -20,9 +22,11 @@ class SurveyIndexView(generic.ListView):
         return Survey.objects.order_by('-title')
     
 class QuestionDetailView(generic.DetailView):
+    """ Detail content of Question. """
     model = Question
     template_name = 'survey/question_detail.html'
     
 class SurveyDetailView(generic.DetailView):
+    """ Detail content of Survey. """
     model = Survey
     template_name = 'survey/survey_detail.html'
