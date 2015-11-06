a68a522078d89a477dbf406cacfbaea629bab229
isitso <ryan033111@gmail.com>
2015-11-05 23:59:27 -0800

    display survey list with questions and their choices

--- survey/urls.py
+++ survey/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import url
-from .views import QuestionIndexView
-
+from .views import QuestionIndexView, SurveyIndexView, QuestionDetailView
+from . import views
 urlpatterns = [
     url(r'^questions/$', QuestionIndexView.as_view()),
+    url(r'^$', SurveyIndexView.as_view()),
+    url(r'^questions/(?P<pk>[0-9]+)/$', QuestionDetailView.as_view()),
+    url(r'^(?P<pk>[0-9]+)/$', views.SurveyDetailView.as_view()),
                ]
--- survey/views.py
+++ survey/views.py
@@ -4,9 +4,25 @@ from .models import Question, Choice, Survey
 
 # Create your views here.
 class QuestionIndexView(generic.ListView):
-    template_name = 'survey/index.html'
+    template_name = 'survey/questions.html'
     context_object_name = 'latest_question_list'
     
     def get_queryset(self):
         """ Return the last five published questions. """
         return Question.objects.order_by('-pub_date')[:5]
+    
+class SurveyIndexView(generic.ListView):
+    template_name = 'survey/surveys.html'
+    context_object_name = 'latest_survey_list'
+    
+    def get_queryset(self):
+        """ Return the last five published surveys. """
+        return Survey.objects.order_by('-title')
+    
+class QuestionDetailView(generic.DetailView):
+    model = Question
+    template_name = 'survey/question_detail.html'
+    
+class SurveyDetailView(generic.DetailView):
+    model = Survey
+    template_name = 'survey/survey_detail.html'
