2c620d458b91130cc9109aa7dddf0a28a35e2a18
isitso <ryan033111@gmail.com>
2015-11-15 15:05:03 -0800

    fix typo in urls.py fix species survey template if survey doesn't exist

--- survey/urls.py
+++ survey/urls.py
@@ -11,6 +11,6 @@ urlpatterns = [
     url(r'^questions/update/(?P<pk>[0-9]+)/$', views.QuestionUpdateView.as_view(), name='question_update'),
     url(r'^species$', views.SpeciesIndexView.as_view(), name='species_index'),
     url(r'^species/create$', views.SpeciesCreateView.as_view(), name='species_create'),
-    url(r'^spceies/(?P<pk>[0-9]+)/$', views.SpeciesDetailView.as_view(), name='species_detail')
+    url(r'^species/(?P<pk>[0-9]+)/$', views.SpeciesDetailView.as_view(), name='species_detail')
 
                ]
